8400deb30a13d7de0acfdab8a31d8274c7685770
Farenyk Maximko <maximfarenik@gmail.com>
2020-07-27 19:22:11 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner str = new Scanner(System.in);
+	    System.out.println( "Your text is here " + str.nextLine ());
+    }
+}
